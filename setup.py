7ea9eedaba2214ad6f2b5669dcc8640c5650ac2f
Krzysztof Klinikowski <kkszysiu@gmail.com>
2022-09-28 00:40:41 +0200

    Bump version to 0.3.13

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as f:
 setup(
     name='restea',
     packages=['restea', 'restea.adapters'],
-    version='0.3.12',
+    version='0.3.13',
     description='Simple RESTful server toolkit',
     long_description=readme_content,
     author='Walery Jadlowski',
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet :: WWW/HTTP',
